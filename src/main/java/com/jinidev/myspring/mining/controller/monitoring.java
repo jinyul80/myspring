36bed15adca6c7f659005350485bd1e10dda4a0a
jinyul80 <jinyul80@gmail.com>
2021-07-16 09:40:46 +0900

    v 0.0.4
    - Dashboard 페이지 템플릿 적용

--- src/main/java/com/jinidev/myspring/mining/controller/monitoring.java
+++ src/main/java/com/jinidev/myspring/mining/controller/monitoring.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mining")
 public class monitoring {
 
-    @GetMapping("dashboard")
+    @GetMapping("/")
     public String dashboard() {
         return "mining/dashboard";
     }
